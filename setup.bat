b79e8bee08d7032979e5d074960a0f65238bba8f
HTPGTDev <98792233+HTPGTDev@users.noreply.github.com>
2023-11-28 20:07:39 +0800

    Update and rename setup.bat to setupNrun.bat

--- setup.bat
+++
@@ -1,5 +0,0 @@
-npm install
-npm install quick.db
-npm install knex
-npx --package typescript tsc --init
-npm run setup
---
+++ setupNrun.bat
@@ -0,0 +1,6 @@
+npm install
+npm install quick.db
+npm install knex
+npm install -D typescript
+npm run setup
+npm run dev
