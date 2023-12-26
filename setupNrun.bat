cf54b42744bd5b590703cdca3cca003119674f21
HTPGTDev <98792233+HTPGTDev@users.noreply.github.com>
2023-12-26 20:38:50 +0800

    Update setupNrun.bat

--- setupNrun.bat
+++ setupNrun.bat
@@ -1,6 +1,6 @@
-call npm install -D -g typescript
-call npm install
+call npm install -g typescript
 call npm install quick.db
 call npm install knex
+call npm install
 call npm run setup
 call npm run dev
